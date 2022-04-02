14198198ec360e1e82c5389528070b05e1c7e153
nar04hamza <102824812+nar04hamza@users.noreply.github.com>
2022-04-02 00:15:05 +0000

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+    {
+        name: "Surah Al-Baqarah",
+        artist: "Abdulrahman Mossad",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "ISurah Al-Baqarah",
+        artist: "Umair Shamim ",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "surah al maidah",
+        artist: "Umair Shamim ",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "surat al choaarae",
+        artist: "Islam Sobhi",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Surah Ibrahim",
+        artist: "Islam Sobhi",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Surah At-Tawbah",
+        artist: "Abdulrahman Mossad",
+        img: "music-6",
+        src: "music-6"
+    },
+]
+
